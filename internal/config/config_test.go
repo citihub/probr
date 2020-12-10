1346f672014f24d4f5917b4e6644f6237c5e2a37
Knight, Eddie <iv.eddieknight@gmail.com>
2020-12-10 10:55:49 -0500

    Handle exclusions specified by config's service pack heirarchy

---
+++ internal/config/config_test.go
@@ -0,0 +1,17 @@
+package config
+
+import (
+	"testing"
+)
+
+func TestInit(t *testing.T)                       {}
+func TestNewConfig(t *testing.T)                  {}
+func TestValidateConfigPath(t *testing.T)         {}
+func TestLogConfigState(t *testing.T)             {}
+func TestAuditDir(t *testing.T)                   {}
+func TestHandleConfigFileExclusions(t *testing.T) {}
+func TestHandleProbeExclusions(t *testing.T)      {}
+func TestAddExclusion(t *testing.T)               {}
+func TestK8sIsExcluded(t *testing.T)              {}
+func TestProbeIsExcluded(t *testing.T)            {}
+func TestScenarioIsExcluded(t *testing.T)         {}
