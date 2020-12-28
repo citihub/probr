ea3d85c80a6b7a5c2eef084a5b5e1c516a00b109
Knight, Eddie <iv.eddieknight@gmail.com>
2020-12-28 06:53:03 -0500

    Added handling for required vars. Implicitly now requires config to be present for a pack to run.

---
+++ service_packs/kubernetes/pack/pack.go
@@ -0,0 +1,30 @@
+package kubernetes_pack
+
+import (
+	"log"
+
+	"github.com/citihub/probr/internal/config"
+	"github.com/citihub/probr/internal/coreengine"
+	"github.com/citihub/probr/internal/utils"
+	"github.com/citihub/probr/service_packs/kubernetes/container_registry_access"
+	"github.com/citihub/probr/service_packs/kubernetes/general"
+	"github.com/citihub/probr/service_packs/kubernetes/iam"
+	"github.com/citihub/probr/service_packs/kubernetes/internet_access"
+	"github.com/citihub/probr/service_packs/kubernetes/pod_security_policy"
+)
+
+func GetProbes() []coreengine.Probe {
+	conf := config.Vars.ServicePacks.Kubernetes
+	if conf.AuthorisedContainerRegistry == "" || conf.UnauthorisedContainerRegistry == "" {
+		file, line := utils.CallerFileLine()
+		log.Printf("[WARN] %s:%v: Ignoring Kubernetes service pack due to required vars not being present.", file, line)
+		return nil
+	}
+	return []coreengine.Probe{
+		container_registry_access.Probe,
+		general.Probe,
+		pod_security_policy.Probe,
+		internet_access.Probe,
+		iam.Probe,
+	}
+}
--- service_packs/service_packs.go
+++ service_packs/service_packs.go
@@ -3,45 +3,22 @@ package service_packs
 import (
 	"path/filepath"
 
-	"github.com/cucumber/godog"
-
 	"github.com/citihub/probr/internal/coreengine"
 	"github.com/citihub/probr/internal/utils"
-	"github.com/citihub/probr/service_packs/kubernetes/container_registry_access"
-	"github.com/citihub/probr/service_packs/kubernetes/general"
-	"github.com/citihub/probr/service_packs/kubernetes/iam"
-	"github.com/citihub/probr/service_packs/kubernetes/internet_access"
-	"github.com/citihub/probr/service_packs/kubernetes/pod_security_policy"
-	"github.com/citihub/probr/service_packs/storage/access_whitelisting"
-	"github.com/citihub/probr/service_packs/storage/encryption_at_rest"
-	"github.com/citihub/probr/service_packs/storage/encryption_in_flight"
+	kubernetes_pack "github.com/citihub/probr/service_packs/kubernetes/pack"
 )
 
-type probe interface {
-	ProbeInitialize(*godog.TestSuiteContext)
-	ScenarioInitialize(*godog.ScenarioContext)
-	Name() string
-}
-
-var packs map[string][]probe
+var packs map[string][]coreengine.Probe
 
 func init() {
-	packs = make(map[string][]probe)
-	packs["kubernetes"] = []probe{
-		container_registry_access.Probe,
-		general.Probe,
-		pod_security_policy.Probe,
-		internet_access.Probe,
-		iam.Probe,
-	}
-	packs["storage"] = []probe{
-		encryption_in_flight.Probe,
-		encryption_at_rest.Probe,
-		access_whitelisting.Probe,
-	}
+	packs = make(map[string][]coreengine.Probe)
+
+	// Kubernetes pack requires the following vars:
+	// AuthorisedContainerRegistry, UnauthorisedContainerRegistry
+	packs["kubernetes"] = kubernetes_pack.GetProbes()
 }
 
-func makeGodogProbe(pack string, p probe) *coreengine.GodogProbe {
+func makeGodogProbe(pack string, p coreengine.Probe) *coreengine.GodogProbe {
 	box := utils.BoxStaticFile(pack+p.Name(), "service_packs", pack, p.Name()) // Establish static files for binary build
 	descriptor := coreengine.ProbeDescriptor{Group: coreengine.Kubernetes, Name: p.Name()}
 	path := filepath.Join(box.ResolutionDir, p.Name()+".feature")
