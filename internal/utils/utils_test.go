e9deeaf62b6ed81a602868889cc0d4ef05de8bdd
lcarrazana <luis.carrazana@citihub.com>
2021-01-07 17:47:51 -0500

    Adding unit tests for FindString and ReplaceBytesValue functions

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -23,3 +23,53 @@ func TestReformatError(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestFindString(t *testing.T) {
+
+	var tests = []struct {
+		slice         []string
+		val           string
+		expectedIndex int
+		expectedFound bool
+	}{
+		{[]string{"the", "and", "for", "so", "go"}, "and", 1, true},
+		{[]string{"the", "and", "for", "so", "go"}, "for", 2, true},
+		{[]string{"the", "and", "for", "so", "go"}, "in", -1, false},
+	}
+
+	for _, c := range tests {
+
+		actualPosition, actualFound := FindString(c.slice, c.val)
+
+		if actualPosition != c.expectedIndex || actualFound != c.expectedFound {
+			t.Errorf("\nCall: FindString(%q,%q)\nResult: %d,%v\nExpected: %d,%v", c.slice, c.val, actualPosition, actualFound, c.expectedIndex, c.expectedFound)
+		}
+	}
+}
+
+func TestReplaceBytesValue(t *testing.T) {
+
+	var tests = []struct {
+		bytes          []byte
+		oldValue       string
+		newValue       string
+		expectedResult []byte
+	}{
+		{[]byte("oldstringhere"), "old", "new", []byte("newstringhere")},                       //Replace a word with no spaces
+		{[]byte("oink oink oink"), "k", "ky", []byte("oinky oinky oinky")},                     //Replace a character
+		{[]byte("oink oink oink"), "oink", "moo", []byte("moo moo moo")},                       //Replace a word with spaces
+		{[]byte("nothing to replace"), "www", "something", []byte("nothing to replace")},       //Nothing to replace due to no match
+		{[]byte(""), "a", "b", []byte("")},                                                     //Empty string
+		{[]byte("Unicode character: ㄾ"), "Unicode", "Unknown", []byte("Unknown character: ㄾ")}, //String with unicode character
+		{[]byte("Unicode character: ㄾ"), "ㄾ", "none", []byte("Unicode character: none")},       //Replace unicode character
+	}
+
+	for _, c := range tests {
+
+		actualResult := ReplaceBytesValue(c.bytes, c.oldValue, c.newValue)
+
+		if string(actualResult) != string(c.expectedResult) {
+			t.Errorf("\nCall: ReplaceBytesValue(%v,%q,%q)\nResult: %v\nExpected: %v", string(c.bytes), c.oldValue, c.newValue, string(actualResult), string(c.expectedResult))
+		}
+	}
+}
