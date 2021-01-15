860132f2988bc32c46eb9c25554b63ba4d62c156
lcarrazana <luis.carrazana@citihub.com>
2021-01-15 16:37:07 -0500

    Removing credential details from config file. Adding two helper scripts to set and check env vars

---
+++ examples/getenvvars.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	fmt.Println("Fetching all env variables")
+	for _, element := range os.Environ() {
+		variable := strings.Split(element, "=")
+		fmt.Println(variable[0], "=>", variable[1])
+	}
+
+	fmt.Println("")
+
+	fmt.Println("Fetching specific env variables")
+	fmt.Println("AZURE_TENANT_ID=>", os.Getenv("AZURE_TENANT_ID"))
+	fmt.Println("AZURE_SUBSCRIPTION_ID=>", os.Getenv("AZURE_SUBSCRIPTION_ID"))
+	fmt.Println("AZURE_CLIENT_ID=>", os.Getenv("AZURE_CLIENT_ID"))
+	fmt.Println("AZURE_CLIENT_SECRET=>", os.Getenv("AZURE_CLIENT_SECRET"))
+}
