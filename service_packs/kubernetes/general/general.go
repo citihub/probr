3f5966b62217196d1290cd00976cb6c5b3ee40ee
Knight, Eddie <iv.eddieknight@gmail.com>
2021-02-19 14:08:44 -0500

    Updated step traces for k8s general probe

--- service_packs/kubernetes/general/general.go
+++ service_packs/kubernetes/general/general.go
@@ -32,27 +32,30 @@ func (s *scenarioState) aKubernetesClusterIsDeployed() error {
 }
 
 //@CIS-5.1.3
+// I inspect the "<Roles / Cluster Roles>" that are configured
 func (s *scenarioState) iInspectTheThatAreConfigured(roleLevel string) error {
-	// Standard auditing logic to ensures panics are also audited
 	stepTrace, payload, err := utils.AuditPlaceholders()
 	defer func() {
+		// Standard auditing logic to ensures panics are also audited
 		s.audit.AuditScenarioStep(stepTrace.String(), payload, err)
 	}()
 
 	if roleLevel == "Cluster Roles" {
+		stepTrace.WriteString("Retrieving instance cluster roles; ")
 		l, e := kubernetes.GetKubeInstance().GetClusterRolesByResource("*")
 		err = e
 		s.wildcardRoles = l
 	} else if roleLevel == "Roles" {
+		stepTrace.WriteString("Retrieving instance roles; ")
 		l, e := kubernetes.GetKubeInstance().GetRolesByResource("*")
 		err = e
 		s.wildcardRoles = l
 	}
 	if err != nil {
-		err = utils.ReformatError("error raised when retrieving '%v': %v", roleLevel, err)
+		err = utils.ReformatError("Could not retrieve role level '%v': %v", roleLevel, err)
 	}
 
-	stepTrace.WriteString(fmt.Sprintf("Ensures that %s are configured. Retains wildcard roles in state for following steps; ", roleLevel))
+	stepTrace.WriteString("Stored any retrieved wildcard roles in state for following steps; ")
 	payload = struct {
 		PodState kubernetes.PodState
 		PodName  string
@@ -70,6 +73,7 @@ func (s *scenarioState) iShouldOnlyFindWildcardsInKnownAndAuthorisedConfiguratio
 	//we strip out system/known entries in the cluster roles & roles call
 	var wildcardCount int
 	//	wildcardCount := len(s.wildcardRoles.([]interface{}))
+	stepTrace.WriteString("Removing known entries from the cluster roles; ")
 	switch s.wildcardRoles.(type) {
 	case *[]v1.Role:
 		wildCardRoles := s.wildcardRoles.(*[]rbacv1.Role)
@@ -80,11 +84,11 @@ func (s *scenarioState) iShouldOnlyFindWildcardsInKnownAndAuthorisedConfiguratio
 	default:
 	}
 
+	stepTrace.WriteString("Validating that no unexpected wildcards were found; ")
 	if wildcardCount > 0 {
 		err = utils.ReformatError("roles exist with wildcarded resources")
 	}
 
-	stepTrace.WriteString("Examines scenario state's wildcard roles; ")
 	payload = struct {
 		PodState kubernetes.PodState
 		PodName  string
@@ -101,16 +105,17 @@ func (s *scenarioState) iAttemptToCreateADeploymentWhichDoesNotHaveASecurityCont
 		s.audit.AuditScenarioStep(stepTrace.String(), payload, err)
 	}()
 
+	stepTrace.WriteString("Create unique pod name; ")
 	cname := "probr-general"
 	podName := kubernetes.GenerateUniquePodName(cname)
-	image := config.Vars.ServicePacks.Kubernetes.AuthorisedContainerRegistry + "/" + config.Vars.ServicePacks.Kubernetes.ProbeImage
 
-	//create pod with nil security context
+	stepTrace.WriteString("Attempt to deploy ProbeImage without a security context; ")
+	image := config.Vars.ServicePacks.Kubernetes.AuthorisedContainerRegistry + "/" + config.Vars.ServicePacks.Kubernetes.ProbeImage
 	pod, podAudit, err := kubernetes.GetKubeInstance().CreatePod(podName, "probr-general-test-ns", cname, image, true, nil, s.probe)
 
+	stepTrace.WriteString(fmt.Sprintf("Ensure failure to deploy returns '%s'; ", kubernetes.UndefinedPodCreationErrorReason))
 	err = kubernetes.ProcessPodCreationResult(&s.podState, pod, kubernetes.UndefinedPodCreationErrorReason, err)
 
-	stepTrace.WriteString("Attempts to create a deployment without a security context. Retains the status of the deployment in scenario state for following steps; ")
 	payload = kubernetes.PodPayload{Pod: pod, PodAudit: podAudit}
 	return err
 }
@@ -127,7 +132,7 @@ func (s *scenarioState) theDeploymentIsRejected() error {
 		err = utils.ReformatError("pod %v was created successfully. Test fail.", s.podState.PodName)
 	}
 
-	stepTrace.WriteString("Validates that a creation error occurred in the previous step; ")
+	stepTrace.WriteString("Validates that an expected creation error occurred in the previous step; ")
 	payload = struct {
 		PodState kubernetes.PodState
 		PodName  string
