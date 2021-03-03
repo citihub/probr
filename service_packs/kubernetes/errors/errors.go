d834e480cbfbe91ab71e37172880cd047cc0dfbe
Knight, Eddie <iv.eddieknight@gmail.com>
2021-03-03 14:17:33 -0500

    Applied PR feedback

--- service_packs/kubernetes/errors/errors.go
+++ service_packs/kubernetes/errors/errors.go
@@ -5,7 +5,6 @@ import "k8s.io/apimachinery/pkg/api/errors"
 // IsStatusCode validates whether an error is a StatusError with a specific status code
 func IsStatusCode(expected int32, err error) bool {
 	if se, ok := err.(*errors.StatusError); ok {
-		//403 is "forbidden"
 		return se.ErrStatus.Code == expected
 	}
 	return false
