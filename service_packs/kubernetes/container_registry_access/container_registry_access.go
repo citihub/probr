3c6a43b5f4ad6e5f9939765c3549bfa0d14adc74
Knight, Eddie <iv.eddieknight@gmail.com>
2021-04-15 13:35:22 -0400

    Added check to ensure pod objects with names are always given a delete signal

--- service_packs/kubernetes/container_registry_access/container_registry_access.go
+++ service_packs/kubernetes/container_registry_access/container_registry_access.go
@@ -225,7 +225,7 @@ func getImageFromConfig(accessLevel bool) string {
 
 func (scenario *scenarioState) createPodfromObject(podObject *apiv1.Pod) (createdPodObject *apiv1.Pod, err error) {
 	createdPodObject, err = conn.CreatePodFromObject(podObject, Probe.Name())
-	if err == nil {
+	if createdPodObject != nil && createdPodObject.ObjectMeta.Name != "" {
 		scenario.pods = append(scenario.pods, createdPodObject.ObjectMeta.Name)
 	}
 	return
